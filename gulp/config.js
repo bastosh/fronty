fb8c5044055980871104083c66c850a282818897
Sébastien Pereda <sebastien.pereda@gmail.com>
2017-10-30 10:03:17 +0100

    Setup

---
+++ gulp/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  // Javascript
+  JS_FILES: [
+  'js/*.js'
+  ],
+
+  JS_DEPS: [
+    'node_modules/jquery/dist/jquery.js'
+  ],
+
+  // CSS
+  CSS_COMPATIBILITY: [
+    'last 2 versions',
+    'ie >= 9',
+    'Android >= 2.3',
+    'ios >= 7'
+  ],
+
+  CSS_DEPS: [
+    'node_modules/bootstrap/dist/css/bootstrap-reboot.css'
+  ],
+
+  DIST_FILES: [
+    'src/favicon.ico',
+    'src/icon.png',
+    'src/tile.png',
+    'src/tile-wide.png',
+    'src/404.html',
+    'src/browserconfig.xml',
+    'src/humans.txt',
+    'src/robots.txt'
+  ],
+
+};
